db026004f741314d55ac82f1dce2e4fb36a82f38
Michal Bartl <michal.bartl@gmail.com>
2025-06-15 15:55:11 +0200

    init

---
+++ test/rocinet.test.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import {describe, it, expect} from "vitest";
+import {Rocinet} from "../src/Rocinet";
+
+describe("Rocinet", () => {
+  const rocinet = new Rocinet(process.env.ROCINET_ACCESS_TOKEN, 10);
+
+  it("load data items", async () => {
+    let counter = 0;
+    const since = new Date("2025-01-01T00:00:00Z");
+    const till = new Date("2025-06-15T00:00:00Z");
+    for await (const item of rocinet.getDataItems("cerv-business-webtrh-used", since, till)) {
+      expect(item.content).toBeTypeOf("object");
+      counter++
+    }
+
+    expect(counter).toBe(22);
+  })
+
+
+});
